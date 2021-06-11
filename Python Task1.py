74880903602d1ff63198defb600ee7e3b3520e95
luitrach <55202426+luitrach@users.noreply.github.com>
2021-06-11 09:17:03 -0700

    Python Task1.py

---
+++ Python Task1.py
@@ -0,0 +1,57 @@
+#Task One-Question 1
+a, b, c = 1, 2.01, "Hello"
+print(a, b, c)
+
+#Question 2
+x= 5+10j
+print(type(int(x)))
+
+#Question 3-using a third variable
+a = 10
+b = 20
+temp = a
+a = b
+b = temp
+print ("a :",a)
+print("b :", b)
+
+#Question 3- without using a third variable
+a = 10
+b = 20
+a = a + b
+b = a - b
+a = a - b
+print (a)
+print (b)
+
+#Question 4- Python 3.x
+a = int(input("Enter a number from 1 to 10"))
+b = int(input("Enter a number from 1 to 10"))
+print (a, b)
+
+#Question 4 - Python 2.x
+a = int(input("Enter a number from 1 to 10"))
+b = int(input("Enter a number from 1 to 10"))
+print 'a, b'
+
+
+#Question 5
+a = int(input("Enter a number from 1 to 10"))
+b = int(input("Enter a number from 1 to 10"))
+z = a + b
+y = z+30
+print(y)
+
+#Question 6
+a = input("Enter a value")
+print("The data type of the input value is ", type(a))
+
+#Question 7 Upper CamelCase, Lower CamelCase, SnakeCase and UPPERCASE
+numberOfClasses = 12
+UpperCamelCase: Numberofclasses = 12
+Lower Camel Case: numberofclasses = 12
+Snakecase: numberofclasses = 12
+UPPERCASE:NUMBEROFCLASSES = 12
+
+#Question 8: 
+#Yes. The value gets changed to a different data type from its current value. For example, if the value entered by user is a string and it is changed to integer, the data type gets changed from string to integer.
